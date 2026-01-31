167920a7d121a9f029b46f5d2663e59664d93ff4
rlop74 <119091421+rlop74@users.noreply.github.com>
2026-01-31 14:45:32 -0500

    connected backend to get data from RDS

--- app/(tabs)/fridge.tsx
+++ app/(tabs)/fridge.tsx
@@ -1,5 +1,5 @@
 import { IconSymbol } from "@/components/ui/icon-symbol";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pressable, Text, TextInput, View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -7,114 +7,77 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 // Hooks
 
+// api
+import { getFridgeItems } from "../../api/fridge.js"
+
 export default function Fridge() {
     const [search, setSearch] = useState("");
-    const foodList = [
-        {
-            id: 1,
-            name: "carrots",
-            quantity: 2,
-            type: "vegetable",
-        },
-        {
-            id: 2,
-            name: "chicken",
-            quantity: 4,
-            type: "meat",
-        },
-        {
-            id: 3,
-            name: "sprite zero",
-            quantity: 3,
-            type: "drink",
-        },
-        {
-            id: 4,
-            name: "apples",
-            quantity: 2,
-            type: "fruit",
-        },
-        {
-            id: 5,
-            name: "ground beef",
-            quantity: 4,
-            type: "meat",
-        },
-        {
-            id: 6,
-            name: "ground pork",
-            quantity: 3,
-            type: "meat",
-        },
-        {
-            id: 7,
-            name: "cabbage",
-            quantity: 2,
-            type: "vegetable",
-        },
-        {
-            id: 8,
-            name: "ribeye",
-            quantity: 4,
-            type: "meat",
-        },
-        {
-            id: 9,
-            name: "coke zero",
-            quantity: 3,
-            type: "drink",
-        },
-    ];
+    const [fridgeItems, setFridgeItems] = useState([]);
+
     const handleCamera = () => {
         alert("Camera has been pressed");
     };
 
+    useEffect(() => {
+        const fetchFridgeItems = async () => {
+            try {
+                const items = await getFridgeItems();
+                setFridgeItems(items);
+            } catch (err) {
+                console.error("Failed to get Fridge Items: ", err);
+                alert("Something went wrong");
+            }
+        };
+
+        fetchFridgeItems();
+    }, []);
+
     return (
         <SafeAreaView className={`flex-1 transition-colors duration-300 gap-1`}>
-                <View className="flex-row justify-between p-3">
-                    <View className="flex-row gap-3">
-                        <View className="bg-[#114924] p-2 rounded-lg">
-                            <IconSymbol
-                                size={27}
-                                name="refrigerator.fill"
-                                color="#14EC5C"
-                            />
-                        </View>
-                        <Text className="text-2xl">Fridge Inventory</Text>
-                    </View>
-                    <Pressable
-                        onPress={handleCamera}
-                        className="p-1 justify-center bg-[#14EC5C] rounded-lg"
-                    >
+            <View className="flex-row justify-between p-3">
+                <View className="flex-row gap-3">
+                    <View className="bg-[#114924] p-2 rounded-lg">
                         <IconSymbol
-                            size={24}
-                            name="camera.viewfinder"
-                            color="black"
+                            size={27}
+                            name="refrigerator.fill"
+                            color="#14EC5C"
                         />
-                    </Pressable>
+                    </View>
+                    <Text className="text-2xl">Fridge Inventory</Text>
                 </View>
-                <View className="relative mx-3 justify-center">
-                    <TextInput
-                        placeholder="Search food items..."
-                        className="border border-gray-400 p-2 pl-12 rounded-xl text-lg"
-                        value={search}
-                    />
+                <Pressable
+                    onPress={handleCamera}
+                    className="p-1 justify-center bg-[#14EC5C] rounded-lg"
+                >
                     <IconSymbol
                         size={24}
-                        name="magnifyingglass"
-                        color="gray"
-                        style={{
-                            position: "absolute",
-                            left: 12,
-                        }}
+                        name="camera.viewfinder"
+                        color="black"
                     />
-                </View>
+                </Pressable>
+            </View>
+            <View className="relative mx-3 justify-center">
+                <TextInput
+                    placeholder="Search food items..."
+                    className="border border-gray-400 p-2 pl-12 rounded-xl text-lg"
+                    value={search}
+                />
+                <IconSymbol
+                    size={24}
+                    name="magnifyingglass"
+                    color="gray"
+                    style={{
+                        position: "absolute",
+                        left: 12,
+                    }}
+                />
+            </View>
 
-                <ScrollView>
-                {foodList.map((food) => {
+            <ScrollView>
+                {fridgeItems?.map((item) => {
                     return (
                         <View
-                            key={food.id}
+                            key={item.id}
                             className="flex-row gap-2 p-2 items-center"
                         >
                             <View className="border rounded-xl p-2 h-20 w-20 justify-center items-center">
@@ -122,9 +85,9 @@ export default function Fridge() {
                             </View>
                             <View className="gap-1">
                                 <Text className="capitalize text-xl">
-                                    {food.name}
+                                    {item.name}
                                 </Text>
-                                <Text>{food.quantity}</Text>
+                                <Text>{item.quantity}</Text>
                             </View>
                         </View>
                     );
